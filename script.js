21baf48e943d8207a90a6f82b9683f6f1e4ff0b8
qaiz1234 <104638348+qaiz1234@users.noreply.github.com>
2023-05-02 03:25:25 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,117 @@
+//console.log("Hello World")
+//var let const
+/*var a=10;
+console.log(a);
+/*var a=20;
+console.log(a);
+let b=23
+
+let a=10;
+let b=20;
+let c=a-b;
+let d=a*b;
+let e=a/b;
+let f=a%b;
+console.log(a+b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+
+let a=10;
+let b="10";
+if(a==b){
+    console.log(true)
+}else{
+    console.log(false)
+}
+*/
+
+let f="Qaiz";
+let g="Naushad";
+let h=f+g;
+console.log(h);
+console.log(`My name is ${f} and ${g}`);
+
+let car ={
+    model:"Celerio",
+    color:"White",
+    price:1000000    
+}
+console.log(car);
+console.log(car.color)
+console.log(`My car is ${car.color} ${car.model}`)
+
+
+let person={
+    name: "Qaiz",
+    college: "CCE",
+    class: "S4",
+    address:{
+        housename:"ijk",
+        street:"tcr"
+}}
+console.log(person.name);
+console.log(person.college);
+console.log(perosn.class);
+console.log(person.address.housename); 
+console.log(`${person.name} ${person.address.housename}`);
+
+//array
+
+let fruits=["apple","Mango","Orange",10]
+fruits[3]="grapes";
+fruits.push("pineapple")
+fruits.unshift("Dragon")
+fruits.pop();
+fruits.shift();
+console.log(fruits)
+
+
+let students =[
+    {name:Arun",age:20},{name:Saya"},age:22}
+
+console.log(student[l].name);
+
+function addTwoNumbers(a,b){
+    .console.log(a+b);
+
+}
+addTwoNumbers(10,20);
+
+function subTwoNumbers(a,b){
+
+console.log(a-b);}
+subTwoNumbers(10,23);
+
+add=(a,b)=>{console.log(a+b);};
+subTwoNumbers(10,30);
+add(45,63);
+add=(a,b)=>
+{let d=a+b;
+    if(d>10)
+    console.log("is greater");
+}
+else    
+{
+    console.log("not greater");
+}
+}    
+add(90,6)
+
+let numbers=[1,2,3,4,5,6,7,8,9,10];
+console.log(numbers);
+print=(a)=>
+{
+    for(i=0;i<=9;i++)
+    {console.log(a[i])}
+}    
+print(numbers);
+
+print=(a)=>{
+    for(let i=0;i<Array.length;i++){
+        console.log(array[i]);
+    }   
+}
+
+print(numbers);
